559f6ba98815d4f842e1ecd23f5f3736d740fb2f
Os Galicia GNOME <o.galicia.0812@gmail.com>
2026-03-12 05:32:07 -0600

    navegador terminado

--- index.php
+++ index.php
@@ -17,11 +17,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&family=Red+Hat+Mono:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./src/css/styles.css">
     <title>Venta de productos Infofast</title>
 </head>
 <body>
     <?php 
-    
+        include_once __DIR__ . "/src/utils/Nav.php";
+
+        
+
     ?>
 </body>
 </html>
